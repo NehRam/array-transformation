a9ab7cd00fe3832b02673290d5fa6368691c0cc4
NehRam <35556142+NehRam@users.noreply.github.com>
2022-04-10 17:41:11 -0600

    Task1
    Task1 of Angular Technical Test

--- index.js
+++ index.js
@@ -1,9 +1,50 @@
 const json = require('./data.json');
 const lodash = require('lodash');
 
-const data = JSON.parse(json);
-console.log(data);
+var data = JSON.parse(json);
+//console.log(data);
 
+var array =[[],[]]
+var array1 = []
+var array2 = []
+var group = null;
+var grouped = "";
+
+function splitTuples(){
+        data = lodash.sortBy(data, ['GroupName'])
+
+    for (let index = 0; index < data.length; index++) {
+        const element = data[index];
+
+        if (element.IdUser != null && element.Id > 0) {
+            if (group == element.GroupName) {
+                grouped = grouped + JSON.stringify(element);
+            }
+            else
+            {
+                if (group == null) {
+                    group = element.GroupName;
+                }
+                else{
+                    array1.push(group + ': ' + grouped);
+                    grouped = [];
+                    grouped = grouped + JSON.stringify(element);
+                }
+            }
+
+            group = element.GroupName;
+        }
+        else{
+            array2.push(element);
+        }
+    }
+}
+
+splitTuples();
+
+array.push(array1, array2)
+
+console.log(array);
 
 // 1. Split the array into a tuple where the first element is those that have IdUser and the second is those objects where IdUser is null or undefined
 //     We know that tuples in js are not available so you can use array of arrays [[], []]
